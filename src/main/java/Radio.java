5ca31217e5cb16a16b72e11ab15eadd3707ff2a5
Greg Poslov <grig.skamsk@mail.ru>
2022-10-30 01:38:54 +0500

    First check

---
+++ src/main/java/Radio.java
@@ -0,0 +1,64 @@
+public class Radio {
+    private int currentVolume;
+    private int currentStation;
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) { // Если меньше нуля уходим
+            return;
+        }
+        if (currentVolume > 10) { // Если больше 10 уходим
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) { // Если меньше нуля уходим
+            return;
+        }
+        if (currentStation > 9) { // Если больше 9 уходим
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void increaseVolume() { // Увеличение звука на 1
+        if (currentVolume < 10) {
+            currentVolume++;
+        } else { // Иначе переключаем на 0
+            currentVolume = 0;
+        }
+    }
+
+    public void reductionVolume() { // Звук больше минимальной уменьшаем на 1
+        if (currentVolume > 0) {
+            currentVolume--;
+        } else { // Иначе переключаем на 10
+            currentVolume = 10;
+        }
+    }
+
+    public void increaseStation() { // Увеличение станции на 1
+        if (currentStation < 9) {
+            currentStation++;
+        } else { // Иначе переключаем на 0
+            currentStation = 0;
+        }
+    }
+
+    public void reductionStation() { // Станция больше минимальной уменьшаем на 1
+        if (currentStation > 0) {
+            currentStation--;
+        } else { // Иначе переключаем на 9
+            currentStation = 9;
+        }
+    }
+}
---
+++ src/test/java/RadioTest.java
@@ -0,0 +1,162 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+
+    @Test
+    public void fromStationZeroToStationOne() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+
+        radio.increaseStation();
+
+        int expected = 1;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void fromStationEightToStationNine() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(8);
+
+        radio.increaseStation();
+
+        int expected = 9;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fromStationNineToStationZero() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+
+        radio.increaseStation();
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fromStationNineToStationEight() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+
+        radio.reductionStation();
+
+        int expected = 8;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fromStationZeroToStationNine() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+
+        radio.reductionStation();
+
+        int expected = 9;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fromStationOneToStationZero() {
+        Radio radio = new Radio();
+        radio.setCurrentStation(1);
+
+        radio.reductionStation();
+
+        int expected = 0;
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fromVolumeZeroToOne() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+
+        radio.increaseVolume();
+
+        int expected = 1;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fromVolumeOneToZero() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(1);
+
+        radio.reductionVolume();
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fromVolumeNineToTen() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(9);
+
+        radio.increaseVolume();
+
+        int expected = 10;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fromVolumeTenToNine() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(10);
+
+        radio.reductionVolume();
+
+        int expected = 9;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fromVolumeTenToZero() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(10);
+
+        radio.increaseVolume();
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fromVolumeZeroToTen() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+
+        radio.reductionVolume();
+
+        int expected = 10;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
